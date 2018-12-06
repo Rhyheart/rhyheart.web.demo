121238f49b04c4f5385ce06f1064e4538a224d9f
Rhyheart <8120183@qq.com>
2018-12-06 10:53:49 +0800

    improve

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,19 +1,61 @@
 <template>
-    <router-view/>
+
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <!-- Aside -->
+        <el-row class="tac">
+          <el-col :span="24">
+            <el-menu @open="handleOpen" @close="handleClose">
+              <el-menu-item index="1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">页面一</span>
+              </el-menu-item>
+              <el-menu-item index="2">
+                <i class="el-icon-setting"></i>
+                <span slot="title">页面二</span>
+              </el-menu-item>
+              <el-menu-item index="3">
+                <i class="el-icon-document"></i>
+                <span slot="title">页面三</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <!-- Main -->
+          <router-view />
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {
-            common : {
-                name : "home",
-                message : "welcome to  home page..."
-            }
+      return {
+        common: {
+          name: "home",
+          message: "welcome to  home page..."
         }
+      };
     },
     mounted() {
-        this.$ele.Message.success(this.common.message);
-    }    
-}
+      this.$ele.Message.success(this.common.message);
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  };
 </script>
